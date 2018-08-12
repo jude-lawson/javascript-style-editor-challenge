c8514090dd20540356c55c4a52bec3dd3247a20f
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-12 13:00:12 -0600

    Implements style editor functionality

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  // your code goes here.
+
+  function updateStyling(selector, property, value) {
+    $(selector).css(property, value);
+  }
+  
+  let form = $('#style_editor');
+  
+  form.submit(function(event) {
+    let selector = $("input[name='selector']").val();
+    let property = $("input[name='property']").val();
+    let value    = $("input[name='value']").val();
+    
+    updateStyling(selector, property, value);
+    event.preventDefault();
+  });
 });
